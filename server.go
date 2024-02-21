e5ebed908905f08995922599c26a2026d00027af
makskhv21 <khvits21@gmail.com>
2024-02-21 18:42:16 +0200

    Add function getFormattedCurrentTime

--- server.go
+++ server.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
 type timeJSON struct {
 	CURRENT_TIME string `json:"time"`
+}
+
+func getCurrentTime(w http.ResponseWriter, r *http.Request) {
+	currentTime := timeJSON{CURRENT_TIME: time.Now().Format(time.RFC3339)}
+	json_data, err := json.Marshal(currentTime)
 }
 
 func main() {}
